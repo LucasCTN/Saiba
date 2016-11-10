a9fc4e75c080cced61b67cce08dd18773f4d088c
LucasCTN <lucascampostn@gmail.com>
2016-11-10 19:06:21 -0200

    Showing related entries, related images e related videos
    Each is in their respective detail view, and uses the tags in common. The more tags, the more on left. Only 5 are shown

--- Saiba/entry/models.py
+++ Saiba/entry/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import Permission, User
 from django.template.defaultfilters import slugify
 from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
-#from home.models import Tag
 
 class Status(models.Model):
     label = models.CharField(max_length=2500, blank=True)
@@ -35,7 +34,7 @@ class Entry(models.Model):
     images_locked           = models.BooleanField(default=False)
     videos_locked           = models.BooleanField(default=False)
     comments_locked         = models.BooleanField(default=False)
-    tags                    = models.ManyToManyField('home.Tag')
+    tags                    = models.ManyToManyField('home.Tag', blank=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
