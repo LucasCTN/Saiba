24ee3dabe2dea43e10235b8a46b7d6275cd3c01c
LucasCTN <lucascampostn@gmail.com>
2016-12-21 19:58:42 -0200

    Test commit

--- Saiba/api/urls.py
+++ Saiba/api/urls.py
@@ -6,7 +6,7 @@ app_name = 'api'
 
 urlpatterns = [
     url(r'^v1/artigo/', views.EntryDetail.as_view(), name='api_article'),
-    url(r'^v1/historicos/(?P<slug>[\w-]+)/$', views.HistoricDetail.as_view(), name='api_historic'),
+    url(r'^v1/historico/(?P<slug>[\w-]+)/$', views.HistoricDetail.as_view(), name='api_historic'),
     url(r'^v1/comment/$', views.CommentDetail.as_view(), name='api_comments'),
     url(r'^v1/comment_page/$', views.CommentPageDetail.as_view(), name='api_comment_page'),
     url(r'^v1/reply/$', views.ReplyDetail.as_view(), name='api_replies'),
