b0f6e9a3f5df580a554180536920f85f2ecc9210
ThiagoCTN <thiagocampostn@gmail.com>
2016-12-22 13:44:15 -0200

    Trending-api merge problem corrected
    For some reason, the merge between profile and trending-api branchs created some problems: some files that were modified wasn't showing differences and no matter what was done, some files were merging even when told to not to. So a manually merge was done. But in this process, some code was lost. But the most important systems were saved.

--- Saiba/staff/views.py
+++ Saiba/staff/views.py
@@ -4,17 +4,8 @@ from entry.models import Entry
 
 # Create your views here.
 def index(request):
-    entries = Entry.objects.all()
 
-    text_form = PostForm(request.POST or None)
-
-    if text_form.is_valid():
-        post = text_form.save(commit=False)
-        post.author = request.user
-        post.save()
+    if not request.user.is_staff:
         return redirect('home:index')
 
-    args = {'entries': entries,
-            'form': text_form}
-
-    return render(request, 'staff/index.html', args)
+    return render(request, 'staff/index.html')
