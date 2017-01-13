9a5e621728d4123d6c64e4ef0388894a286cfa9a
ThiagoCTN <thiagocampostn@gmail.com>
2017-01-13 18:18:40 -0200

    Tags advancing
    Now tags is appearing when you select a search or press ',' (comma), allowing using existing tags in database or creating new. Is not implemented the additiong of tags yet.

--- Saiba/gallery/static/gallery/scripts.js
+++ Saiba/gallery/static/gallery/scripts.js
@@ -1,4 +1,6 @@
 ﻿$(function () {
+    var justKeyDown = false;
+
     $('#search-tags').focusin(function () {
         $('#search-tag-results').show();
 
@@ -7,19 +9,35 @@
                 $('#search-tag-results').hide();
             }
         });
+
+        $('#search-tags').keydown(function (e) {
+            if (e.keyCode == 188 && !justKeyDown) {
+                selectedTag($('#search-tags').val())
+            }
+
+            justKeyDown = true;
+        });
     });
 
     $('#search-tags').focusout(function () {
         window.setTimeout(function () { $('#search-tag-results').hide() }, 100);
     });
 
-    $('#search-tags').keyup(function () {
-        $.ajax({
-            type: "GET",
-            url: "../pesquisar-tags/?q=" + $('#search-tags').val(),
-            success: searchTagSuccess,
-            dataType: 'html'
-        });
+    $('#search-tags').keyup(function (e) {
+        justKeyDown = false;
+
+        if (e.keyCode == 188) {
+            $('#search-tags').val('');
+        }
+        else
+        {
+            $.ajax({
+                type: "GET",
+                url: "../pesquisar-tags/?q=" + $('#search-tags').val(),
+                success: searchTagSuccess,
+                dataType: 'html'
+            });
+        }
     });
 });
 
@@ -27,7 +45,14 @@ function searchTagSuccess(data) {
     $('#search-tag-results').html(data);
 
     $('.tags-result').click(function (event) {
-        var tag = $(event.target);
-        $('.search-tag-display ul').append('<li>' + tag.text().trim() + '</li>');
+        selectedTag($(event.target).text());
     });
+}
+
+function selectedTag(tag) {
+    var tagString = tag.trim();
+
+    $('.search-tag-display ul').append('<li>' + tagString + '</li>');
+    $('#tags-selected').val($('#tags-selected').val() + ',' + tagString);
+    $('#search-tags').val('');
 }
