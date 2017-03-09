58ac59900fd3a1d014c93be278901d5ccaf18425
LucasCTN <lucascampostn@gmail.com>
2017-03-09 15:07:36 -0300

    Refrain from use outside requesting to get api results
    This makes it more testing-friendly.

--- Saiba/Saiba/utils.py
+++ Saiba/Saiba/utils.py
@@ -1,21 +1,13 @@
 from rest_framework.reverse import reverse
-import urllib, urllib2, json
+from api.views import TrendingDetail
 
 def is_valid_direction(direction):
     return int(direction) in [-1, 0, 1]
 
 def get_trending_entries(request):
-    entry_trending_url = reverse('api:api_trending', request=request) + "?type=entry"
-
-    entry_trending_result = urllib2.urlopen(entry_trending_url).read()
-    trending_entries = json.loads(entry_trending_result)
-
+    trending_entries = TrendingDetail.as_view()(request, "entry").data
     return trending_entries[:5]
 
 def get_popular_galleries(request):
-    gallery_trending_url = reverse('api:api_trending', request=request) + "?type=gallery"
-
-    gallery_trending_result = urllib2.urlopen(gallery_trending_url).read()
-    trending_galleries = json.loads(gallery_trending_result)
-
+    trending_galleries = TrendingDetail.as_view()(request, "gallery").data
     return trending_galleries[:5]
