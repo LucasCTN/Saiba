54e379159fda8a68bfcc74f4240c13a1b32781b3
LucasCTN <lucascampostn@gmail.com>
2017-03-09 15:08:36 -0300

    Updating tests method names and creating a test example

--- Saiba/entry/tests.py
+++ Saiba/entry/tests.py
@@ -3,6 +3,7 @@ import django
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 from entry.models import Entry
+from profile.models import Profile
 from entry.views import create_entry
 
 # TODO: Configure your database in settings.py and sync before running tests.
@@ -12,18 +13,21 @@ class EntryTestCase(TestCase):
     """Tests for the entry views."""
 
     @classmethod
-    def setUpClass(cls):
-        cls.factory = RequestFactory()
-        cls.user = User.objects.create_user(
-            username='Joao', email='joao@…', password='top_secret')
+    def setUpTestData(self):
         django.setup()
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='Joao', email='joao@…', password='top_secret')
+        profile = Profile.objects.create(user=self.user)
+        profile.save()
 
     def test_create_entry(self):
         """
         Tests creating a entry.
         """
-        request = self.factory.get('/entrada/nova-entrada/')
+        request = self.factory.get('/entrada/nova-entrada')
         request.user = self.user
 
         response = create_entry(request)
+
+        print response.content
         self.assertEqual(response.status_code, 200)
