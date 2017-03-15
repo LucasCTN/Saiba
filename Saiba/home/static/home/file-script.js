4e980a15a93976de3587cf0c37f2770c03a80168
ThiagoCTN <thiagocampostn@gmail.com>
2017-03-14 21:22:31 -0300

    Updated the tag and file input

---
+++ Saiba/home/static/home/file-script.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $("#id_icon").change(function (e) {
+        $('.custom-file-field').html($('#id_icon').val());
+    });
+
+    $(".custom-file-control").click(function (e) {
+        $('.form-control-file').trigger('click');
+    });
+});
