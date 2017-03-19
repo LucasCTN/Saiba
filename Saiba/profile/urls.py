f3b2f88a495fc491b729a47b5a0d51944b6d09a9
LucasCTN <lucascampostn@gmail.com>
2017-03-19 01:36:46 -0300

    Profile pages
    Video page does not have icons working.

--- Saiba/profile/urls.py
+++ Saiba/profile/urls.py
@@ -5,4 +5,7 @@ app_name = 'profile'
 
 urlpatterns = [
     url(r'^(?P<name_slug>[\w-]+)/$', views.detail, name='detail'),
+    url(r'^(?P<name_slug>[\w-]+)/editoria', views.editorships, name='editorships'),
+    url(r'^(?P<name_slug>[\w-]+)/imagens', views.images, name='images'),
+    url(r'^(?P<name_slug>[\w-]+)/videos', views.videos, name='videos'),
 ]
--- Saiba/profile/views.py
+++ Saiba/profile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
+from entry.models import Entry
+from gallery.models import Image, Video
 
 def index(request, name_slug):
     profile = get_object_or_404(Profile, slug=name_slug)
@@ -12,3 +14,24 @@ def detail(request, name_slug):
 
     context = { "profile": profile, 'type': 'profile', "id": profile.user.id}
     return render(request, 'profile/detail.html', context)
+
+def editorships(request, name_slug):
+    profile = get_object_or_404(Profile, slug=name_slug)
+    entries = Entry.objects.filter(author=profile.user)
+
+    context = { "profile": profile, 'type': 'profile', "id": profile.user.id, "entries": entries}
+    return render(request, 'profile/editorship.html', context)
+
+def images(request, name_slug):
+    profile = get_object_or_404(Profile, slug=name_slug)
+    images = Image.objects.filter(author=profile.user)
+
+    context = { "profile": profile, 'type': 'profile', "id": profile.user.id, "images": images}
+    return render(request, 'profile/images.html', context)
+
+def videos(request, name_slug):
+    profile = get_object_or_404(Profile, slug=name_slug)
+    videos = Video.objects.filter(author=profile.user)
+
+    context = { "profile": profile, 'type': 'profile', "id": profile.user.id, "videos": videos}
+    return render(request, 'profile/videos.html', context)
