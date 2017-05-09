e0b6dc541b0fe9d0c858d7c4c52c0942cc4a1e92
LucasCTN <lucascampostn@gmail.com>
2017-05-09 00:32:04 -0300

    Pylint is complaining

--- Saiba/feedback/views.py
+++ Saiba/feedback/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 import feedback.services
 
 def comment_page(request):
-    id   = request.GET.get('id') or ""
-    type = request.GET.get('type') or ""
+    model_id = request.GET.get('id') or ""
+    model_type = request.GET.get('type') or ""
     child_limit = request.GET.get('child_limit') or 5
     chain = request.GET.get('chain') or ""
     page = int(request.GET.get('page')) or 1
 
     comments_limit = 10
 
-    comments = feedback.services.get_target_comment_page(id, type, page, comments_limit)
-    pages_total = feedback.services.get_target_page_count(id, type, comments_limit)
+    comments = feedback.services.get_target_comment_page(model_id, model_type, page, comments_limit)
+    pages_total = feedback.services.get_target_page_count(model_id, model_type, comments_limit)
     pages_total = int(pages_total)
 
     user_can_delete = request.user.profile.HasPermission('delete_comment')
 
-    args = { 'comments' : comments, 'pages_total': range(1, pages_total + 1), 'current_page': page, 'chain': chain, 
-            'child_limit': child_limit, 'user_can_delete': user_can_delete }
+    args = {'comments' : comments, 'pages_total': range(1, pages_total + 1), 'current_page': page,
+            'chain': chain, 'child_limit': child_limit, 'user_can_delete': user_can_delete}
 
     return render(request, 'feedback/comment_page.html', args)
