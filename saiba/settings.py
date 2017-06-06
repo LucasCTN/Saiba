972f61b444de5223f9f3a4bfca38348a26adaa9e
LucasCTN <lucascampostn@gmail.com>
2017-06-06 13:48:36 -0300

    Changing debug to true for staging

--- saiba/settings.py
+++ saiba/settings.py
@@ -27,7 +27,7 @@ SITE_ID = 1
 SECRET_KEY = '5bf35afd-fed9-40b1-8119-6a7a621b79b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
