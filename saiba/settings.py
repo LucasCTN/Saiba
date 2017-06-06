87b87cabb4eec1b98bc10a7c481d70fe31a8302b
LucasCTN <lucascampostn@gmail.com>
2017-06-06 19:52:58 -0300

    Defining LOGIN_URL for auth decorators
    Fixes anonymous users receiving error 500 when trying to edit entries.

--- saiba/settings.py
+++ saiba/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import posixpath
 import dj_database_url
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,6 +33,8 @@ DEBUG = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
+
+LOGIN_URL = reverse_lazy('home:login')
 
 # Application definition
 
