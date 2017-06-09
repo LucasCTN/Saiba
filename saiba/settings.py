c4d005fa265badfd8261933bce586f37691b0c3f
LucasCTN <lucascampostn@gmail.com>
2017-06-09 01:09:59 -0300

    Showing errors on entry edit and  installing back django.contribs.sites

--- saiba/settings.py
+++ saiba/settings.py
@@ -59,8 +59,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.sitemaps',
-    #'django.contrib.sites',
 ]
 
 MIDDLEWARE_CLASSES = [
