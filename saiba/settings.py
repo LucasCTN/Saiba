467db1bbfa21f780679201248d987a001d0ae657
LucasCTN <lucascampostn@gmail.com>
2017-06-09 17:46:03 -0300

    And... adding again...

--- saiba/settings.py
+++ saiba/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'storages',
     # Django apps
-    #'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
